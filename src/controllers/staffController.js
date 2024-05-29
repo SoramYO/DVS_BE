ab1f77c74903aa03e16d4526acb7b94d2f404bcf
SoramYO <ngoxuanson121@gmail.com>
2024-05-30 03:04:32 +0700

    update for fe

--- src/controllers/staffController.js
+++ src/controllers/staffController.js
@@ -2,7 +2,7 @@ var staffService = require('../services/staffService');
 
 
 let handleChangeProcess = async (req, res) => {
-    let message = await staffService.changeProcess(req.body);
+    let message = await staffService.changeProcess(req.body, req.query);
     return res.status(200).json(message)
 }
 
