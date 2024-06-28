b50badc1da2835ff9e69b3d91180398d99aad753
Xuan Son <ngoxuanson121@gmail.com>
2024-06-28 13:05:54 +0700

    show service

--- src/routes/web.js
+++ src/routes/web.js
@@ -3642,6 +3642,8 @@ let initWebRoutes = (app) => {
 
   router.get("/bill", verifyToken, managerController.handleGetBill);
 
+  router.get("/user-service", verifyToken, userController.handleGetUserService);
+
   router.get("/icon", (req, res) => {
     res.send('😀😃😄😁😆😅🤣😂');
   });
